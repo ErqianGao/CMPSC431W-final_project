0bda73a36bec12b285b823fe53f04a4db6565ca9
Erqian Gao <ezg5260@psu.edu>
2021-04-27 01:07:25 +0800

    added some pages

---
+++ HomePage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>homepage</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<h1>Welcome to Book Store!</h1>
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])){
+    echo '<a href="register.php"><button>Register</button></a> ';
+    echo '<a href="login.php"><button>Login</button></a>';
+}else{
+    echo '<a href="searchBook"><button>search for a book</button></a> ';
+    echo '<form class="dform" action="controller.php" method="post"><button name="logout" value="logout">Logout</button></form>';
+    echo '<i><b> Hello '.$_SESSION['user'].'!</i></b>';
+}
--- database_controller.php
+++ database_controller.php
@@ -1,1 +1,67 @@
 <?php
+class database_controller
+{
+    private $DB;
+
+    public function __construct()
+    {
+        $servername = 'localhost';
+        $dbname = "project1_erqiangao";
+        $user = 'root';
+        $password = '';
+        try {
+            $this->DB = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $user, $password);
+            $this->DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo ("connect successfully!");
+        } catch (PDOException $e) {
+            echo('Error establishing Connection');
+            exit ();
+        }
+    }
+
+    function __destruct(){
+        $conn = NULL;
+    }
+
+    public function getBooks( $name ){
+        if( $name == null ){
+            $stmt = $this->DB->prepare("SELECT * FROM books");
+            $stmt->execture();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        else{
+            $stmt = $this->DB->prepare("SELECT * FROM books WHERE (title LIKE '%$name%') OR (authors LIKE '%$name%') OR (isbn LIKE '%$name%')");
+            $stmt->execute();
+            return $stmt->fetchALL(PDO::FETCH_ASSOC);
+        }
+    }
+
+    public function newComment( $login_name, $isbn, $text ){
+        $stmt = $this->DB->prepare("INSERT INTO comments(login_name,isbn,text) VALUES ('$login_name', '$isbn', '$text')");
+        $stmt->execute();
+    }
+
+    public function getComment( $isbn ){
+        $stmt = $this->DB->prepare("SELECT * FROM comments WHERE isbn = '$isbn'");
+        $stmt->execute();
+        return $stmt->fetchALL(PDO::FETCH_ASSOC);
+    }
+
+    public function createNewManager( $name, $password ){
+        $stmt = $this->DB->prepare( "INSERT INTO managers(login_name, password) VALUES ('$name', '$password')" );
+        $stmt->execute();
+    }
+
+    public function login($login_name, $password){
+        $stmt= $this->DB->prepare( "SELECT login_name, password FROM users WHERE login_name='".$login_name."'AND password='".$password."';");
+        $stmt->execute();
+        $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($array)===0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+
+?>
